f9b22b7354f47f26b59167f7df27e9fd04fd068c
HaoLiangPao <hflsforeverhao@gmail.com>
2020-06-05 18:44:52 -0400

    Simple back-end API with hard-coded data was created

--- express_crash_course/routes/api/members.js
+++ express_crash_course/routes/api/members.js
@@ -35,5 +35,40 @@ router.post("/", (req, res) => {
     res.send(members);
   }
 });
+// Update a member
+router.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id)); // req.params.id returns strings
+  if (found) {
+    const updateMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updateMember.name ? updateMember.name : member.name;
+        member.email = updateMember.email ? updateMember.email : member.email;
+        res.json({ msg: "Member updated", member });
+      }
+    });
+    res.json(members); // .json() methods can take care of things like json.stringif()
+  } else {
+    res.status(400).json({
+      msg: `MEMBER NOT FOUND: No members with the id of ${req.params.id}`,
+    });
+  }
+});
+// Delete a member
+router.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id)); // req.params.id returns strings
+  if (found) {
+    res.json({
+      msg: "Member deleted",
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      ),
+    }); // .json() methods can take care of things like json.stringif()
+  } else {
+    res.status(400).json({
+      msg: `MEMBER NOT FOUND: No members with the id of ${req.params.id}`,
+    });
+  }
+});
 
 module.exports = router;
