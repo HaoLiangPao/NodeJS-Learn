d060ea4b2f77c2f44ac6af4a6dc69e7f5b89987c
HaoLiangPao <hflsforeverhao@gmail.com>
2020-06-09 23:11:57 -0400

    Basic route backbone created

--- dev_camper_api/server.js
+++ dev_camper_api/server.js
@@ -6,6 +6,13 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Create Routes
+app.get("/", (req, res) => {
+  // res.send can send json objects as well, express will automatically convert it
+  res.sendStatus(400);
+  res.send("");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
