e64a2c04484a9b98113342f0e599d5e788c7ef3a
HaoLiangPao <hflsforeverhao@gmail.com>
2020-06-10 18:37:28 -0400

    Mongoose installed, variables neede for database connection stored in config files, unhandle promise exception hanlled in server.js, will add color package into the backend API so logs will be more readable.

--- dev_camper_api/server.js
+++ dev_camper_api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // Middleware files
 // const logger = require("./middleware/logger"); // custom logger
@@ -11,7 +12,9 @@ const bootcamps = require("./routes/bootcamps");
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// --------- App running -----------
 const app = express();
+connectDB();
 
 // Dev running middleware
 if (process.env.NODE_ENV === "development") {
@@ -21,8 +24,14 @@ if (process.env.NODE_ENV === "development") {
 // Get routes defined
 app.use("/api/v1/bootcamps", bootcamps);
 
+// Listening to the port
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}!`);
+});
+// Handle unhandle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
 });
